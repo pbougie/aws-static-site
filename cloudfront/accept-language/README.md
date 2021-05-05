7450df0c90950e80e59abae13d97e8d2d18042b1
Patrick Bougie <patrick@bougie.ca>
2021-05-05 19:35:30 -0400

    Update "accept-language" function

--- cloudfront/accept-language/README.md
+++ cloudfront/accept-language/README.md
@@ -1,20 +1,16 @@
-# AWS Static Site
-## CloudFront Function
-### Accept-Language
+# CloudFront Function
+## Accept-Language
 
-Redirects root path to `/fr/` if `Accept-Language` starts with `fr`. Otherwise it redirects to `/en/`.
+Redirects root path to `/fr/` if `Accept-Language` starts with `fr`. Otherwise it redirects to `/en/`. You can modify the function for your site’s languages.
 
-Create a new function:
+Create CloudFront function:
 
-- Function name: `cloudfront-accept-language`
-- Runtime: `Node.js`
-- Execution role: **Use existing role** `cloudfront-lambda@edge-role`
+- Function Name: `accept-language`
+- Comment: `Redirect to language`
+- Source Code: [accept-language.js](accept-language.js)
 
-Add the code from **[cloudfront-accept-language.js](cloudfront-accept-language.js)** and deploy.
-
-Add a **CloudFront** trigger:
+Publish then associate with one or more CloudFront distributions:
 
 - Distribution: select the distribution ID
-- CloudFront event: `Viewer Request`
-
-Deploy to Lambda@Edge and link to CloudFront distribution. You might have to modify the function for your site’s languages. You can create an additional CloudFront behavior with path pattern `/` to attach this function if you have another `Viewer Request` event.
+- Event Type: `Viewer Request`
+- Cache Behavior: `/`
--- cloudfront/accept-language/accept-language.js
+++ cloudfront/accept-language/accept-language.js
@@ -1,39 +1,28 @@
-// Lambda@Edge
-// Function name: cloudfront-accept-language
-// CloudFront event: Viewer Request
+// Function Type: CloudFront Function
+// Function Name: accept-language
+// CloudFront Event: Viewer Request
 
-exports.handler = async (event) => {
+function handler(event) {
 
-  const request = event.Records[0].cf.request;
-  const headers = request.headers;
-  let language = 'en'; // Default language
+  var request = event.request;
+  var uri = request.uri;
+  var headers = request.headers;
 
-  if (request.uri == '/') {
+  var language = 'en'; // Default language
 
-    if (typeof headers['accept-language'] !== 'undefined') {
-      const supportedLanguages = headers['accept-language'][0].value;
-      if (supportedLanguages.startsWith('fr')) {
-        language = 'fr';
-      }
-      console.log('Supported Languages: ', supportedLanguages);
-      console.log('Language: ', language);
+  if (typeof headers['accept-language'] !== 'undefined') {
+    var supportedLanguages = headers['accept-language'].value;
+    if (supportedLanguages.startsWith('fr')) {
+      language = 'fr';
     }
-
-    return {
-      status: '302',
-      statusDescription: 'Found',
-      headers: {
-        location: [{
-          key: 'Location',
-          value: '/' + language + '/'
-        }]
-      }
-    };
-
-  } else {
-
-    return request;
-
   }
 
-};
+  return {
+    statusCode: 302,
+    statusDescription: 'Found',
+    headers: {
+      location: { value: '/' + language + '/' }
+    }
+  };
+
+}
